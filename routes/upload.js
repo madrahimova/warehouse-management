ded731980de4ae391ab0bb2c11dcd5ff18dad61c
meowbyte <darya.mad.ry@gmail.com>
2021-04-24 19:09:51 +0300

    Fix upload route

--- routes/upload.js
+++ routes/upload.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET uploads */
 router.get('/', function(req, res, next) {
-    if (!req.body.count)
-        res.send('count is not specified');
+    if (!req.query.count)
+        res.send('error: count is not specified');
     else
-        res.send('done uploading ' + req.body.count + ' item(s)');
+        res.send('done uploading ' + req.query.count + ' item(s)');
 });
 
 module.exports = router;
