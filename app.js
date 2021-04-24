8d11c05eafc806ff57c30147b50b9f10e8bfd23e
meowbyte <darya.mad.ry@gmail.com>
2021-04-24 18:32:36 +0300

    Fix upload route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var MongoClient = require('mongodb').MongoClient;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var uploadRouter = require('./routes/upload');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/upload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
